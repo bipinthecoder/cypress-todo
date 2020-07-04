38680d851bb8e6fe19645bf1d3dc181a329ac441
Bipin Babu <bipinbabu@live.com>
2020-07-04 17:50:47 +0530

    added logic for green colur

--- cypress/integration/task-form-initialise.spec.js
+++ cypress/integration/task-form-initialise.spec.js
@@ -21,4 +21,15 @@ describe('App Initialisation', () => {
         cy.get('[type="checkbox"]')
         .should("have.class","check-box")
     })
+    it("item list should have class success",()=>{
+        cy.get(".task-list div")
+        // cy.get('[type="checkbox"]')
+        .should("have.class","notSuccess")
+    })
+    it("item lshould have color green on completed status",()=>{
+        // cy.get(".success")
+        cy.get('[type="checkbox"]').check()
+        // .cy.type("{enter}")
+        .should("have.class","text-success")
+    })
 });
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import TaskItem from "./TaskItem";
 
-const TaskItem = ( {data} )=>
-  <li>
-    <div >
-      <input type="checkbox" className="check-box"/>
-      <label>
-        {data.title}
-      </label>
-    </div>
-  </li>
+// const TaskItem = ( {data} )=>
+//   <li className="success">
+//     {/* <div className="success" > */}
+//     <div >
+//       <input type="checkbox" className="check-box"/>
+//       <label>
+//         {data.title}
+//       </label>
+//     </div>
+//   </li>
 
 const TaskList = props =>
   <ul className="task-list">
